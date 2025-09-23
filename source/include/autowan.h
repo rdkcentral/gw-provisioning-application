7594e7240ea3fbec255b24708ceda2713d15c7ed
Sreehari RamanjulaVenkata <sreehari.ramanjulavenkata@sky.uk>
2025-09-23 16:06:29 +0100

    Add the XF10 product common flag
    Signed-off-by: Sreehari Ramanjula<220073682+RAMANJULASREEHARI@users.noreply.github.com>

--- source/include/autowan.h
+++ source/include/autowan.h
@@ -4,7 +4,7 @@
 #define DOCSIS_INF_NAME "cm0"
 #if defined (_XB7_PRODUCT_REQ_)
 #define ETHWAN_INF_NAME "eth3"
-#elif defined (_XER5_PRODUCT_REQ_) || defined (_SCER11BEL_PRODUCT_REQ_)
+#elif defined (_XER5_PRODUCT_REQ_) || defined (_SCER11BEL_PRODUCT_REQ_) || defined (_SCXF11BFL_PRODUCT_REQ_)
 #define ETHWAN_INF_NAME "eth4"
 #elif defined (_CBR2_PRODUCT_REQ_)
 #define ETHWAN_INF_NAME "eth5"
